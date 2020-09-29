08c858116ebc7938ecf299855755055d866e514e
emanuele5180 <macina5180>
2020-09-29 16:05:37 +0200

    migration refresh

---
+++ database/migrations/2020_09_29_134551_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('title',64);
+            $table -> text('text',64);
+            $table -> string('category',64);
+            $table -> integer('like');
+            $table -> integer('dislike');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -3,9 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+
+    <link rel="stylesheet" href={{ asset }}("/css/app.css")>
   </head>
   <body>
-    
+
     @include("partials.header")
     @yield('content')
     @include("partials.footer")
